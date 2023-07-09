3bc3d17c64c3950666ecab52f04bdb77c4a8762b
Jussa <juscelino.carvalho@gmail.com>
2023-07-09 18:55:18 -0300

    Entrvistas e mais músicas...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-/*import 'node_modules/glightbox/dist/js/glightbox.js';*/
-//import { GLightbox } from 'glightbox';
 
 @Component({
   selector: 'app-root',
@@ -10,25 +8,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   title = "Jussa's Website";
-  lightbox:any;
-
-  ngOnInit(){       
-    //lightbox settings
-    this.lightbox = GLightbox({
-      'href': 'https://youtu.be/N-dbHwQUFag',
-      'type': 'video',
-      'source': 'youtube', //vimeo, youtube or local
-      'width': 900,
-      'autoplayVideos': true,
-    });
- }
 
 
 }
-function GLightbox(arg0: {
-  href: string; type: string; source: string; //vimeo, youtube or local
-  width: number; autoplayVideos: boolean;
-}): any {
-  throw new Error('Function not implemented.');
-}
-
--- src/app/entrevistas/entrevistas.component.ts
+++ src/app/entrevistas/entrevistas.component.ts
@@ -6,24 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./entrevistas.component.css']
 })
 
-/*
-function MovieCard("{movie}" : "Props"){
-  return(
-  <div>
-      <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
-      <div className="dsmovie-card-bottom-container">
-          <h3>{movie.title}</h3>
-          <MovieScore count={movie.count} score={movie.score}/>
-          <Link to={`/form/${movie.id}`}>
-              <div className="btn btn-primary dsmovie-btn">Avaliar</div>
-          </Link>
-      </div>
-  </div>
-
-  );*/
 
 export class EntrevistasComponent {
-  public videoPhenom100 = "../../assets/video/Phenom100.mp4"
-  public videoDecolagemCessna210 = "../../assets/video/DecolagemLow.mp4"
-  public videoJussaPousoSkylane = "../../assets/video/EuPousandoSkylane.mp4"
+  public RCNPart1 = "../../assets/video/TVCProgressoEmPauta1.mp4"
+  public RCNPart2 = "../../assets/video/TVCProgressoEmPauta2.mp4"
+  public RadioCacula1 = "../../assets/video/RadioCacula1.mp4"
+  public RCNFull = "../../assets/video/RCN_FULL-2.mp4"
 }
